1f47785d4d64bf9b744ba5460b2693cd13056197
thiag <thiagotomaznunes.9908@outlook.com>
2025-07-09 23:03:00 -0300

    first commit

---
+++ src/test/java/br/com/rinha_3/rinha_backend/RinhaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rinha_3.rinha_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
