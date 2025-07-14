80961a6e1bcb27964739a65939e693f0ac4a09b5
thiago.nunes <thiago.nunes@perbank.com.br>
2025-07-14 13:23:59 -0300

    add primeira chamadas default e fallback on rest client

--- src/main/java/br/com/rinha_3/rinha_backend/client/ProcessPaymentClient.java
+++ src/main/java/br/com/rinha_3/rinha_backend/client/ProcessPaymentClient.java
@@ -1,7 +1,11 @@
 package br.com.rinha_3.rinha_backend.client;
 
 import br.com.rinha_3.rinha_backend.payment.entity.Payment;
+import br.com.rinha_3.rinha_backend.payment.entity.Processor;
+import br.com.rinha_3.rinha_backend.payment.entity.Status;
 import br.com.rinha_3.rinha_backend.payment.repository.PaymentRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -12,9 +16,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 public class ProcessPaymentClient {
 
     private PaymentRepository paymentRepository;
+    private final ObjectMapper objectMapper;
 
-    public ProcessPaymentClient(PaymentRepository paymentRepository) {
+    public ProcessPaymentClient(PaymentRepository paymentRepository, ObjectMapper objectMapper) {
         this.paymentRepository = paymentRepository;
+        this.objectMapper = objectMapper;
     }
 
     @Transactional
@@ -22,14 +28,55 @@ public class ProcessPaymentClient {
 
         var restClient = RestClient.create();
 
-        var response = restClient
-                .post()
-                .uri("http://localhost:8002/payments")
-                .body(new PaymentRequestProcessor(payment))
-                .contentType(APPLICATION_JSON)
-                .retrieve()
-                .body(Object.class);
+        try {
 
-        System.out.println(response);
+            var responseDefault = restClient
+                    .post()
+                    .uri("http://localhost:8001/payments")
+                    .body(toJson(payment))
+                    .contentType(APPLICATION_JSON)
+                    .retrieve()
+                    .toBodilessEntity();
+
+            if (responseDefault.getStatusCode().is2xxSuccessful()) {
+                payment.updateStatus(Status.PAID);
+                payment.setProcessor(Processor.DEFAULT);
+            }
+
+        } catch (Exception e) {
+
+            try {
+                var responseFallback = restClient
+                        .post()
+                        .uri("http://localhost:8002/payments")
+                        .body(toJson(payment))
+                        .contentType(APPLICATION_JSON)
+                        .retrieve().toBodilessEntity();
+
+                if (responseFallback.getStatusCode().is2xxSuccessful()) {
+
+                    payment.updateStatus(Status.PAID);
+                    payment.setProcessor(Processor.FALLBACK);
+                }
+
+            } catch (Exception e2) {
+
+                System.out.println("deu ruim até no fallback! msg:"+e.getMessage()+"pagamento:"+payment.getCorrelationId());
+            }
+        }
+    }
+
+    private String toJson(Payment payment) {
+        try {
+
+            return objectMapper.writeValueAsString(
+                    new PaymentRequestProcessor(payment));
+
+        } catch (JsonProcessingException e) {
+
+            System.out.println("err to convert class in json");
+
+        }
+        return null;
     }
 }
